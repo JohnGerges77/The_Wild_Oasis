44621666f6f682e509bb3954e8e4b8f072167d99
JOHNGERGES <90909659+JohnGerges77@users.noreply.github.com>
2024-12-21 00:36:35 +0200

    Update UpdateProfileForm.js

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -38,7 +38,7 @@ function UpdateProfileForm({ guest, children }) {
       <div className="space-y-2">
         <div className="flex items-center justify-between">
           <label htmlFor="nationality">Where are you from?</label>
-          <Image
+          <img
           width={34}
           height={24}
             src={countryFlag}
